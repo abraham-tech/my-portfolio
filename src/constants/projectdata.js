ffb487e7b8eab62c152d9d9c2973d76d815c4d3e
abraham <abraham.mathewos.meja@gmail.com>
2024-01-23 21:11:35 -0600

    Add flask blog to project list

--- src/constants/projectdata.js
+++ src/constants/projectdata.js
@@ -1,7 +1,25 @@
 import portfolio from "../assets/portfolio.jpeg";
+import mazes from "../assets/mazes.png"
+import flaskBlog from "../assets/flaskBlog.png"
 const projects = [
   {
     id: 1,
+    image: flaskBlog,
+    name: "Flask blog app",
+    description:
+        "This is a Flask blog application which includes features such as user registration, login, updating profiles with profile pictures, creating, updating and deleting blog posts.",
+    link: "https://github.com/abraham-tech/Flask_Blog"
+  },
+  {
+    id: 2,
+    image: mazes,
+    name: "Maze game",
+    description:
+        "This exciting game is based on python using simple yet intuitive functionalities which includes generating a random maze and finding its way out.",
+    link: "https://github.com/abraham-tech/uu_mazes/tree/master"
+  },
+  {
+    id: 3,
     image: portfolio,
     name: "Portfolio site",
     description:
